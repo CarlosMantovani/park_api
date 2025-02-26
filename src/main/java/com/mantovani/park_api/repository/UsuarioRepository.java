245524664ceabebd8c924587518166e5218cbf19
Carlos Mantovani <mantovanicarloshenrique@gmail.com>
2025-02-26 15:53:45 -0300

    feat: create repository

---
+++ src/main/java/com/mantovani/park_api/repository/UsuarioRepository.java
@@ -0,0 +1,4 @@
+package com.mantovani.park_api.repository;
+
+public interface UsuarioRepository extends org.springframework.data.jpa.repository.JpaRepository<com.mantovani.park_api.entity.Usuario, java.lang.Long> {
+  }
