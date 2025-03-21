1bff2d7cb96fbda0bf736acea04d1b0f186d6dca
Carlos Mantovani <mantovanicarloshenrique@gmail.com>
2025-03-21 17:38:11 -0300

    feat: create internationalization of validation messages

--- src/main/java/com/mantovani/park_api/web/exception/ApiExceptionHandler.java
+++ src/main/java/com/mantovani/park_api/web/exception/ApiExceptionHandler.java
@@ -2,7 +2,9 @@ package com.mantovani.park_api.web.exception;
 
 import com.mantovani.park_api.exception.*;
 import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.MessageSource;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -14,7 +16,10 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @Slf4j
 @RestControllerAdvice
+@RequiredArgsConstructor
 public class ApiExceptionHandler {
+
+    private final MessageSource messageSource;
 
     @ExceptionHandler(AccessDeniedException.class)
     public ResponseEntity<ErrorMessage> accessDeniedException(AccessDeniedException ex, HttpServletRequest request) {
@@ -60,6 +65,7 @@ public class ApiExceptionHandler {
         return ResponseEntity
                 .status(HttpStatus.UNPROCESSABLE_ENTITY)
                 .contentType(MediaType.APPLICATION_JSON)
-                .body(new ErrorMessage(request, HttpStatus.UNPROCESSABLE_ENTITY, "Campo(s) invalido(s)", result));
+                .body(new ErrorMessage(request, HttpStatus.UNPROCESSABLE_ENTITY, messageSource.getMessage("message.invalid.field", null, request.getLocale()
+                ), result, messageSource));
     }
 }
